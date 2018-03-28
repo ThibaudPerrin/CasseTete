dcf93d2feadcae2ab27207dd44eaa4850a80c531
Thibaud <thibaud.perrin6@gmail.com>
2018-03-28 16:59:56 +0200

    Constructeur Grille

--- src/casseTete/Grille.java
+++ src/casseTete/Grille.java
@@ -10,14 +10,27 @@ public class Grille extends Observable {
 	
 	int lastI;
 	int lastJ;
+	int Longueur, largeur;
 	
-	void startDD(int i , int j ) {
+	public Grille(int longueur, int largeur) {
+		setLongueur(longueur);
+		setLargeur(largeur);
+		tab = new Case[longueur][largeur];
+	}
+	
+	private void setLongueur(int longueur) {
+		this.Longueur = longueur;
+	}
+	private void setLargeur(int largeur) {
+		this.largeur = largeur;
+	}
+	public void startDD(int i , int j ) {
 		System.out.println("startDD : " + i + "-" + j);
         setChanged();
         notifyObservers();
 	}
 	
-	void parcoursDD(int i , int j){
+	public void parcoursDD(int i , int j){
 		    lastI = i;
 	        lastJ = j;
 	        System.out.println("parcoursDD : " + i + "-" + j);
@@ -25,7 +38,7 @@ public class Grille extends Observable {
 	        notifyObservers();
 	}
 	
-	void stopDD(int i , int j) {
+	public void stopDD(int i , int j) {
         System.out.println("stopDD : " + i + "-" + j + " -> " + lastI + "-" + lastJ);
         setChanged();
         notifyObservers();
